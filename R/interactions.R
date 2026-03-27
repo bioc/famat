449de54b299006f6f65e3b84c160cf897e64d49b
MathieuCharlesINRAE <mathieu.charles@inrae.fr>
2026-03-27 11:22:29 +0100

    Fix merge conflict >>>>> in code and bump version to 1.21.4

--- R/interactions.R
+++ R/interactions.R
@@ -141,7 +141,6 @@ filter_inter<-function(inter){
 ##Input : path_enrich results with KEGG, Reactome and Wikipathways.
 interactions<-function(listk, listr, listw){
 #    all_compounds_chebi <- read.csv("data/all_compounds_chebi.csv")
->>>>>>> RELEASE_3_22
     resmetak<-listk[[1]]; resgenek<-listk[[2]]; resmetar<-listr[[1]]
     resgener<-listr[[2]]; resmetaw<-listw[[1]]; resgenew<-listw[[2]]
     genes<-listk[[3]]; meta<-listk[[4]]
